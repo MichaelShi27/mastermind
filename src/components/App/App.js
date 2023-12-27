8af7fcea70ace45a26b5aef4c2fedb8ddfe4d18a
Michael Shi <michaelshi03@gmail.com>
2023-12-27 10:56:24 -0800

    Add options skeleton & alter styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,32 @@
 import useApp from './useApp.js';
 import Input from '../Input/Input.js';
 import History from '../History/History.js';
-import { Message, PlayAgainButton, AppContainer } from '../styles.js';
+import { 
+  Message, PlayAgainButton, GuessContainer, OptionsContainer, OptionsButton, OptionsInput
+} from '../styles.js';
 
 const App = () => {
   const appVariables = useApp();
-  const { gameOver, message, remainingGuesses, startNewGame } = appVariables;
+  const { number, gameOver, message, remainingGuesses, startNewGame } = appVariables;
 
-  return (
-    <AppContainer>
+  return (<>
+    <GuessContainer>
       {!gameOver && <Input {...appVariables} />}
       <Message>{message}</Message>
       {gameOver && <PlayAgainButton onClick={startNewGame}>Play Again</PlayAgainButton>}
       <div>Remaining guesses: {remainingGuesses}</div>
-      <History {...appVariables } />
-    </AppContainer>
-  );
+      <History {...appVariables} />
+    </GuessContainer>
+    <OptionsContainer>
+      Options:
+      <OptionsButton>Show Hint</OptionsButton>
+      <label>Enter number of digits to guess (4 - 8): </label>
+      <OptionsInput placeholder={number ? number.length : ''} />
+      <label>Enter number of turns (5 - 15): </label>
+      <OptionsInput />
+      <OptionsButton>Add Timer</OptionsButton>
+    </OptionsContainer>
+  </>);
 };
 
 export default App;
